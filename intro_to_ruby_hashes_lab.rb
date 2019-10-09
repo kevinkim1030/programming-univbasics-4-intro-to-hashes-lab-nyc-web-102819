4b1956b7280c661878f771dbfceb11c8c03caa26
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-09 21:52:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-    read_from_hash = {:name => "Steve", 'name' => 'Tzvi'}
+    read_from_hash = {:name => "Steve", :'name' => 'Tzvi'}
     return read_from_hash[:name]
   # return the correct value using the hash and key parameters
 end
