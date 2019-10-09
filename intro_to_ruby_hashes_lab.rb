63180890b3ffa4901c2775c328dec31120f6dbc0
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-09 22:08:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 
 def read_from_hash(key, value)
     read_from_hash = {:name => "Steve", 'name' => "Tzvi"}
-    return read_from_hash[:name]
+    return read_from_hash['name']
   # return the correct value using the hash and key parameters
 end
 
